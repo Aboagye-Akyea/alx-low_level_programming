71b81a887b861a6be626d20e33f76d14f59dc37b
Aboagye-Akyea <kofiaboagyeakyea@gmail.com>
2023-03-18 01:05:30 +0000

    add task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point A-Z a-z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'z'; low++)
+	putcharlow();
+	putchar('\n');
+
+	return (0);
+}
