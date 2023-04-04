e6657466741e7d8ba99b3b7291deb2bf7b547985
Aboagye-Akyea <kofiaboagyeakyea@gmail.com>
2023-04-04 05:11:35 +0000

    rename 100 task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * set_string - sets the value of a pointer to a char
  * @s: pointer to change
@@ -7,7 +6,6 @@
  *
  * Return: void
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
